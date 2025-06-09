571b8584acebe7223616d7c685f932c30fe601e8
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-09 16:09:22 +0900

    [update] カメラ回転操作を追加

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -3,17 +3,32 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] Vector3 horizontalPosition;
+    [SerializeField] Vector3 rotate;
+    [SerializeField] float distance;
     [SerializeField] float speed = 5f;
+    [SerializeField] float rotateSpeed = 5f;
 
     Vector2 moveInput;
+    float rotateInput;
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
+    public void OnRotate(InputValue value)
+    {
+        rotateInput = value.Get<float>();
+    }
+
     private void Update()
     {
-        transform.position += speed * Time.deltaTime * new Vector3(moveInput.x, 0, moveInput.y);
+        var dt = Time.deltaTime;
+        horizontalPosition += speed * dt * (Quaternion.Euler(0, rotate.y, 0) * new Vector3(moveInput.x, 0, moveInput.y));
+        rotate.y += rotateInput * rotateSpeed * dt;
+        
+        transform.rotation = Quaternion.Euler(rotate);
+        transform.position = horizontalPosition - transform.forward * distance;
     }
 }
