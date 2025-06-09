967bcbdeb2d46a82b3a81a0104ff37f15b1c3f13
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-09 22:57:51 +0900

    [feat] TimeScaleによるポーズ機能を追加

--- Assets/Scripts/System/PauseManager.cs
+++ Assets/Scripts/System/PauseManager.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField] InputActionReference pause;
+
+    bool isPausing = false;
+
+    private void Start()
+    {
+        pause.action.performed += _ =>
+        {
+            if (!isPausing)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        };
+    }
+
     public void Pause()
     {
-
+        isPausing = true;
+        Time.timeScale = 0;
     }
 
     public void Resume()
     {
-
+        isPausing = false;
+        Time.timeScale = 1;
     }
 }
