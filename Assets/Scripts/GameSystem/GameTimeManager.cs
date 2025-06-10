b7d3b682db08026104edbd2b083f604f5108e520
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-10 20:43:25 +0900

    [add] GameTimeManager.csを追加

---
+++ Assets/Scripts/GameSystem/GameTimeManager.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// ゲーム内時間を管理するクラス
+/// </summary>
+public class GameTimeManager : Singleton<GameTimeManager>
+{
+    private float _currentTime = 100;
+    public static float CurrentTime => Instance._currentTime;
+}
