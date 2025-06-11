10b9f63378740abd56aa05fa55a83b162e92be68
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-11 18:09:13 +0900

    [add] UnitStatsViewのテスト用にUnitStats.csを追加

--- Assets/Scripts/Unit/UI/UnitStatsView.cs
+++ Assets/Scripts/Unit/UI/UnitStatsView.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// ユニットの基本情報表示
 /// </summary>
 public class UnitStatsView : MonoBehaviour
 {
-    
+    [SerializeField] UnitStats unitStats;
+    [SerializeField] Text testStatsText;
+
+    private void Update()
+    {
+        UpdateView();
+    }
+
+    private void UpdateView()
+    {
+        testStatsText.text = unitStats.TestStats.ToString("F2");
+    }
 }
