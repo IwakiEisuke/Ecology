3db984a131728f98f1fd3ad9a2f2e17fe130bbf6
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-25 02:29:35 +0900

    [update] UnitControllerの処理をUnitBrainを介したものに変更

--- Assets/Scripts/Unit/UnitControl/UnitController.cs
+++ Assets/Scripts/Unit/UnitControl/UnitController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class UnitController : MonoBehaviour
 {
-    [SerializeField] NavMeshAgent agent;
-    private void Update()
+    [SerializeField] UnitSelector unitSelector;
+    [SerializeField] UnitBrain unitBrain;
+
+    private void Start()
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo))
-        {
-            agent.SetDestination(hitInfo.point);
-        }
+        InputManager.MouseInput.OnClicked += Attention;
+    }
+
+    private void Attention()
+    {
+        unitBrain.SetTarget(unitSelector.HoveredTarget);
     }
 }
